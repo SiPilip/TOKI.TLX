8f244b2b63cc13139e68182800226fca546296d6
Philifs Bryan Sipahutar <115935707+SiPilip@users.noreply.github.com>
2022-10-17 13:22:25 +0700

    Main.java
    Deskripsi
    Pak Dengklek meminta Anda membuat sebuah program sebagai berikut. Program akan menerima sebuah bilangan bulat N. Jika N adalah bilangan bulat positif, cetak positif. Jika N adalah bilangan bulat negatif, cetak negatif. Selain itu (yakni jika N adalah nol), cetak nol.
    
    Format Masukan
    Sebuah berisi sebuah bilangan bulat N.
    
    Format Keluaran
    Sebuah baris berisi keluaran sesuai permintaan soal.
    
    Contoh Masukan 1
    2
    Contoh Keluaran 1
    positif
    Contoh Masukan 2
    0
    Contoh Keluaran 2
    nol
    Contoh Masukan 2
    -2
    Contoh Keluaran 2
    negatif

---
+++ 1. Pemrograman Dasar/05. Percabangan/D. If Then Else/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        if (n > 0) {
+            System.out.println("positif");
+        }
+        if (n == 0) {
+            System.out.println("nol");
+        }
+        if (n < 0){
+            System.out.println("negatif");
+        }
+
+    }
+}
