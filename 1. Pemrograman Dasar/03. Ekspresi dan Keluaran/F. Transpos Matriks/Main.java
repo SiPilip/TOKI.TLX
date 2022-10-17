1525b9a973ea2e7646366050d3a0d57b87c3b24d
Philifs Bryan Sipahutar <115935707+SiPilip@users.noreply.github.com>
2022-10-17 13:18:47 +0700

    Create Main.java
    Deskripsi
    Matriks berukuran 3 × 3 adalah kumpulan 9 bilangan yang disusun dalam bentuk sebagai berikut. Karakter-karakter 'a' - 'i' menyatakan bilangan-bilangan bulat yang merupakan anggota-anggota dari matriks tersebut.
    
    a b c
    d e f
    g h i
    Transpos dari suatu matriks A yang berukuran 3 × 3, dilambangkan dengan AT, adalah matriks yang susunan anggota-anggotanya diubah menjadi seperti berikut.
    
    a d g
    b e h
    c f i
    Pak Dengklek memberikan Anda sebuah matriks A yang berukuran 3 × 3. Tentukan transpos dari matriks A tersebut.
    
    Format Masukan
    Tiga buah baris berisi anggota-anggota matriks A sesuai format di atas.
    
    Format Keluaran
    Tiga buah baris berisi anggota-anggota transpos dari matriks A sesuai format di atas.
    
    Contoh Masukan
    1 2 3
    4 5 6
    7 8 9
    Contoh Keluaran
    1 4 7
    2 5 8
    3 6 9

---
+++ 1. Pemrograman Dasar/03. Ekspresi dan Keluaran/F. Transpos Matriks/Main.java
@@ -0,0 +1,30 @@
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a,b,c,d,e,f,g,h,i;
+
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+
+        d = scan.nextInt();
+        e = scan.nextInt();
+        f = scan.nextInt();
+
+        g = scan.nextInt();
+        h = scan.nextInt();
+        i = scan.nextInt();
+
+        System.out.print(a+" ");
+        System.out.print(d+" ");
+        System.out.print(g+"\n");
+        System.out.print(b+" ");
+        System.out.print(e+" ");
+        System.out.print(h+"\n");
+        System.out.print(c+" ");
+        System.out.print(f+" ");
+        System.out.print(i+"\n");
+    }
+}
